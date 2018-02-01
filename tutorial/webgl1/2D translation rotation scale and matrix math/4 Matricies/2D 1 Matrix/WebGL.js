edd1a313f6cee69b2ff4a00c2518c26849a4e3b3
Ian Lo <ilo5@my.bcit.ca>
2018-02-01 10:56:40 -0800

    finished matricies 1

--- tutorial/webgl1/2D translation rotation scale and matrix math/4 Matricies/2D 1 Matrix/WebGL.js
+++ tutorial/webgl1/2D translation rotation scale and matrix math/4 Matricies/2D 1 Matrix/WebGL.js
@@ -12,11 +12,15 @@ function main() {
 	attribute vec2 a_position;
 	
 	uniform vec2 u_resolution;
+	/*
 	uniform vec2 u_translation;
 	uniform vec2 u_rotation;
 	uniform vec2 u_scale;
+	*/
+	uniform mat3 u_matrix;
 
 	void main() {
+		/*
 		//Scale the position
 		vec2 scaledPosition = a_position * u_scale;
 
@@ -28,6 +32,9 @@ function main() {
 
 		// Add in translation
 		vec2 position = rotatedPosition + u_translation;
+		*/
+
+		vec2 position = (u_matrix * vec3(a_position, 1)).xy;
 
 		//pixels to range 0 to 1
 		vec2 zeroToOne = position /u_resolution;
@@ -62,23 +69,27 @@ function main() {
     var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
     var colorLocation = gl.getUniformLocation(program, "u_color");
 
+	/*
 	var translationLocation = gl.getUniformLocation(program, "u_translation");
 
 	var rotationLocation = gl.getUniformLocation(program, "u_rotation");
 
 	var scaleLocation = gl.getUniformLocation(program, "u_scale");
+	*/
+
+	var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
 	var positionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 	setGeometry(gl);
 
-	var translation = [130, 30];
+	var translation = [130,30];
 	
-	var rotation = [];
+	//var rotation = [];
 	var angleDeg = -45;
 	var angleRad = angleDeg * Math.PI / 180;
-	rotation[0] = Math.sin(angleRad);
-	rotation[1] = Math.cos(angleRad);
+	//rotation[0] = Math.sin(angleRad);
+	//rotation[1] = Math.cos(angleRad);
 
 	var scale = [0.2, 0.2];
 
@@ -115,6 +126,7 @@ function main() {
     
 		gl.uniform4fv(colorLocation, color);
 		
+		/*
 		//set the translation
 		gl.uniform2fv(translationLocation, translation);
 
@@ -123,7 +135,20 @@ function main() {
 
 		//set the scale
 		gl.uniform2fv(scaleLocation, scale);
-    
+		*/
+
+		//compute the matricies
+		var translationMatrix = m3.translation(translation[0],translation[1]);
+		var rotationMatrix = m3.rotation(angleRad);
+		var scaleMatrix = m3.scaling(scale[0],scale[1]);
+
+		//Multiply the matricies
+		var matrix = m3.multiply(translationMatrix, rotationMatrix);
+		matrix = m3.multiply(matrix, scaleMatrix);
+
+		//set the matrix
+		gl.uniformMatrix3fv(matrixLocation, false, matrix);
+		
         var primitiveType =gl.TRIANGLES;
         var offset = 0;
         var count = 18;
@@ -257,4 +282,64 @@ function setGeometry(gl) {
 		gl.STATIC_DRAW
 	);
 }
+
+var m3 = {
+	translation : function(tx, ty) {
+		return [
+			1,  0,  0,
+			0,  1,  0,
+			tx, ty, 1,
+		];
+	},
+	rotation: function (angleInRadians) {
+		var c = Math.cos(angleInRadians);
+		var s = Math.sin(angleInRadians);
+		return [
+			c, -s,  0,
+			s,  c,  0,
+			0,  0,  1,
+		];
+	},
+	scaling: function (sx, sy) {
+		return [
+			sx, 0,  0,
+			0,  sy, 0,
+			0,  0,  1,
+		];
+	},
+	multiply: function (a, b) {
+		var a00 = a[0 * 3 + 0];
+		var a01 = a[0 * 3 + 1];
+		var a02 = a[0 * 3 + 2];
+		var a10 = a[1 * 3 + 0];
+		var a11 = a[1 * 3 + 1];
+		var a12 = a[1 * 3 + 2];
+		var a20 = a[2 * 3 + 0];
+		var a21 = a[2 * 3 + 1];
+		var a22 = a[2 * 3 + 2];
+
+		var b00 = b[0 * 3 + 0];
+		var b01 = b[0 * 3 + 1];
+		var b02 = b[0 * 3 + 2];
+		var b10 = b[1 * 3 + 0];
+		var b11 = b[1 * 3 + 1];
+		var b12 = b[1 * 3 + 2];
+		var b20 = b[2 * 3 + 0];
+		var b21 = b[2 * 3 + 1];
+		var b22 = b[2 * 3 + 2];
+		return [
+			b00 * a00 + b01 * a10 + b02 * a20,
+			b00 * a01 + b01 * a11 + b02 * a21,
+			b00 * a02 + b01 * a12 + b02 * a22,
+
+			b10 * a00 + b11 * a10 + b12 * a20,
+			b10 * a01 + b11 * a11 + b12 * a21,
+			b10 * a02 + b11 * a12 + b12 * a22,
+
+			b20 * a00 + b21 * a10 + b22 * a20,
+			b20 * a01 + b21 * a11 + b22 * a21,
+			b20 * a02 + b21 * a12 + b22 * a22,
+		];
+	},
+};
 main();
